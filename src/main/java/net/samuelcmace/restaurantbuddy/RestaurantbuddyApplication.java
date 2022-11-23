c51e0134dc902332adc1cca9f42517674726a750
Samuel Mace <samuelcmace@gmail.com>
2022-11-23 10:46:10 -0600

    Initial Commit

---
+++ src/main/java/net/samuelcmace/restaurantbuddy/RestaurantbuddyApplication.java
@@ -0,0 +1,13 @@
+package net.samuelcmace.restaurantbuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantbuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantbuddyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/samuelcmace/restaurantbuddy/RestaurantbuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package net.samuelcmace.restaurantbuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantbuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
