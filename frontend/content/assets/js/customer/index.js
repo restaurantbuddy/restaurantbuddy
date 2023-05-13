12801cfb71ec95a2d57acde6d05743f9a85e92de
Samuel Mace <samuelcmace@gmail.com>
2023-05-13 12:18:03 -0500

    Rename frontend-static to frontend

--- frontend/content/assets/js/customer/index.js
+++ frontend/content/assets/js/customer/index.js
@@ -2,7 +2,6 @@ import '../include/js.cookie.min.js';
 import {userNotAuthorized} from '../shared/user-not-authorized.js';
 import {checkCookieConsent} from "../shared/eu-cookie-prompt.js";
 
-
 (function () {
 
     if (checkCookieConsent() === true) {
